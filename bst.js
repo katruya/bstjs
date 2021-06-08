254c8d9a69b86abd2fd3a711bbd2dddbd4f237fa
katruya <kybryce4@gmail.com>
2021-06-08 12:45:27 -0500

    finished first draft of insert method

---
+++ bst.js
@@ -0,0 +1,44 @@
+import Node from './binary_node';
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        let newNode = new Node(data);
+        
+        if (this.getRootNode() === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let current = this.getRootNode();
+        while (current) {
+            if (data === current.data) {
+                return undefined;
+            }
+            else if (data < current.data) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this; 
+                }
+                current = current.left;
+            }
+            else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+            
+        }
+
+        
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+}
