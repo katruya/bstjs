9b3df2fc8c48dd9068d058b106d9b902d93f7259
katruya <kybryce4@gmail.com>
2021-06-08 12:08:04 -0500

    completed Node class - represents a node in the BST

---
+++ binary_node.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    getLeft() {
+        return this.left;
+    }
+
+    getRight() {
+        return this.right;
+    }
+}
+
+export default binary_node;
